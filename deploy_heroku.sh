1f7f8c363223a331f4dd599312c67ae4a0f345a8
Ryoma Abe <ryoma.abe@fuller.co.jp>
2016-03-28 10:49:29 +0900

    Create script to deploy to heroku

---
+++ deploy_heroku.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+function git_dirty {
+  git add -A .
+  [[ $(git diff --cached --shortstat 2> /dev/null | tail -n1) != "" ]] && echo "*"
+}
+
+COMMIT_REV=$(cat .git/ORIG_HEAD)
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+if [ -n "$(git_dirty)" ]; then
+    GIT_DIRTY=true
+    git stash save
+fi
+
+rm -rf .tmp
+mkdir -p .tmp
+
+cp -r dist .tmp/public
+cp server/app.js server/package.json .tmp/
+
+git checkout staging
+
+rm -rf package.json app.js public
+cp -r .tmp/public ./
+cp .tmp/package.json .tmp/app.js ./
+
+git add -A
+git commit -m "${COMMIT_REV}"
+git push heroku staging:master
+
+git checkout ${BRANCH}
+
+if [ -n "${GIT_DIRTY}" ]; then
+    git stash pop
+fi
